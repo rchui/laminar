d8b7e86761ac757a8d771b993ba05dfee4d1b337
Ryan Chui <ryanc@23andme.com>
2021-01-26 00:18:27 -0800

    Remove auto logging config because it messes with docker containers.

--- laminar/__init__.py
+++ laminar/__init__.py
@@ -1,20 +1,2 @@
-import logging.config
-
 from laminar.components import Flow as Flow  # noqa
 from laminar.components import Response as Response  # noqa
-
-logging.config.dictConfig(
-    {
-        "version": 1,
-        "formatters": {"laminar": {"format": "%(levelname)8s | %(message)s"}},
-        "handlers": {
-            "laminar": {
-                "level": "INFO",
-                "formatter": "laminar",
-                "class": "logging.StreamHandler",
-                "stream": "ext://sys.stdout",
-            },
-        },
-        "loggers": {"laminar": {"handlers": ["laminar"], "level": "INFO", "propagate": True}},
-    }
-)
