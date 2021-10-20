ffd30199c6fc7201d13fee9af501df8ba47a677a
Ryan Chui <ryanc@23andme.com>
2021-10-20 11:19:33 -0700

    Jsonify archive and modularize models

--- laminar/configurations/datasources.py
+++ laminar/configurations/datasources.py
@@ -1,7 +1,6 @@
 """Configuraitons for laminar data sources."""
 
 import hashlib
-import os
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Any
@@ -9,24 +8,27 @@ from typing import Any
 import cloudpickle
 from smart_open import open
 
-from laminar import models
+from laminar.models import Archive, Artifact
 
 
 @dataclass(frozen=True)
 class DataSource:
     root: str
 
+    def __post_init__(self) -> None:
+        object.__setattr__(self, "root", self.root.rstrip("/"))
+
     def _read(self, uri: str) -> Any:
         # Read the archive
         with open(uri, "r") as file:
-            archive = models.Archive.parse_raw(file.read())
+            archive = Archive.parse_raw(file.read())
 
         # Read the artifact value
-        with open(archive.uri(self.root), "rb") as file:
+        with open(archive.artifact.uri(self.root), "rb") as file:
             return cloudpickle.load(file)
 
     def read(self, *, flow: str, execution: str, layer: str, artifact: str) -> Any:
-        """Read an artifact from the laminar archive.
+        """Read an artifact from the laminar datasource.
 
         Args:
             uri (str): URI of the artifact to read.
@@ -35,12 +37,12 @@ class DataSource:
             Any: Value of the artifact.
         """
 
-        return self._read(os.path.join(self.root, flow, execution, layer, artifact))
+        return self._read(Archive.uri(root=self.root, flow=flow, execution=execution, layer=layer, artifact=artifact))
 
-    def _write(self, uri: str, artifact: Any) -> None:
-        contents = cloudpickle.dumps(artifact)
-        archive = models.Archive(
-            length=len(artifact) if hasattr(artifact, "__len__") else None,
+    def _write(self, uri: str, value: Any) -> None:
+        contents = cloudpickle.dumps(value)
+        archive = Archive(
+            length=len(value) if hasattr(value, "__len__") else None,
             hexdigest=hashlib.sha256(contents).hexdigest(),
         )
 
@@ -49,18 +51,18 @@ class DataSource:
             file.write(archive.json())
 
         # Write the artifact value
-        with open(archive.uri(self.root)) as file:
+        with open(archive.artifact.uri(self.root), "wb") as file:
             file.write(contents)
 
     def write(self, *, flow: str, execution: str, layer: str, artifact: str, value: Any) -> None:
-        """Write an artifact to the laminar archive.
+        """Write an artifact to the laminar datasource.
 
         Args:
             uri (str): URI of the artifact to write.
             artifact (Any): Value of the artifact.
         """
 
-        self._write(os.path.join(self.root, flow, execution, layer, artifact), value)
+        self._write(Archive.uri(root=self.root, flow=flow, execution=execution, layer=layer, artifact=artifact), value)
 
 
 @dataclass(frozen=True)
@@ -68,18 +70,14 @@ class Local(DataSource):
     root: str = str(Path.cwd() / ".laminar")
 
     def write(self, *, flow: str, execution: str, layer: str, artifact: str, value: Any) -> None:
-        uri = os.path.join(self.root, flow, execution, layer, artifact)
+        uri = Archive.uri(root=self.root, flow=flow, execution=execution, layer=layer, artifact=artifact)
+
         Path(uri).parent.mkdir(parents=True, exist_ok=True)
-        (Path(self.root) / "archive").mkdir(parents=True, exist_ok=True)
+        Path(Artifact.root(self.root)).mkdir(parents=True, exist_ok=True)
 
         self._write(uri, value)
 
 
 @dataclass(frozen=True)
 class S3(DataSource):
-    bucket: str
-    prefix: str
-
-    @property
-    def root(self) -> str:
-        return f"s3://{self.bucket}/{self.prefix.strip('/')}"
+    ...
--- laminar/models.py
+++ laminar/models.py
@@ -4,9 +4,28 @@ from typing import Optional
 from pydantic import BaseModel
 
 
+class Artifact(BaseModel):
+    archive: "Archive"
+
+    @staticmethod
+    def root(root: str) -> str:
+        return os.path.join(root, "artifacts")
+
+    def uri(self, root: str) -> str:
+        return os.path.join(self.root(root), f"{self.archive.hexdigest}.gz")
+
+
 class Archive(BaseModel):
     length: Optional[int]
     hexdigest: str
 
-    def uri(self, root: str) -> str:
-        return os.path.join(root, "archive", f"{self.hexdigest}.gz")
+    @staticmethod
+    def uri(*, root: str, flow: str, execution: str, layer: str, artifact: str) -> str:
+        return os.path.join(root, flow, execution, layer, f"{artifact}.json")
+
+    @property
+    def artifact(self) -> Artifact:
+        return Artifact(archive=self)
+
+
+Artifact.update_forward_refs()
