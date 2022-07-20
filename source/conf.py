5467bbbc731b9a84af1873d7c26ab5d7e295b21f
rchui <rchui@users.noreply.github.com>
2022-07-20 23:39:01 +0000

    deploy: 9af360a35cb93fa3d6d153ed934e9111ab7d233b

--- source/conf.py
+++ source/conf.py
@@ -37,6 +37,7 @@ extensions = [
     "sphinx.ext.autosummary",
     "sphinx.ext.githubpages",
     "sphinx.ext.napoleon",
+    "sphinx_immaterial",
 ]
 autosummary_generate = True  # Make and include autosummary files
 
@@ -54,7 +55,7 @@ templates_path = ["_templates"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_immaterial"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
