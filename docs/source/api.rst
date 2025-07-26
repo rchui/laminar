eb4caeee3252312671f577344d907dfe8a934825
Ryan Chui <ryan.w.chui@gmail.com>
2025-07-25 18:37:46 -0700

    clean repo

--- docs/source/api.rst
+++
@@ -1,6 +0,0 @@
-.. autosummary::
-    :toctree: api
-    :template: module.rst
-    :recursive:
-
-    laminar
--- laminar/__init__.py
+++
@@ -1,7 +0,0 @@
-"""The workflow framework that works for you."""
-
-from laminar.components import Execution, Flow, Layer, Parameters
-from laminar.version import VERSION
-
-__all__ = ["Execution", "Flow", "Layer", "Parameters"]
-__version__ = VERSION
